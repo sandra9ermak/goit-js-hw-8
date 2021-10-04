849786cb5728652534bb255b804190934b4bb533
sandra9ermak <sandra1ermak@gmail.com>
2021-10-04 15:45:29 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,10 @@ import throttle from "lodash.throttle";
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on('timeupdate', throttle(data => {
-    localStorage.setItem("videoplayer-current-time", data.seconds);
+player.on('timeupdate', throttle(function (data) {
+    localStorage.setItem("videoplayer-current-time", data);
   }, 1000));
 
-//player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
 const time = localStorage.getItem("videoplayer-current-time");
 const res = time ? time : 0;
